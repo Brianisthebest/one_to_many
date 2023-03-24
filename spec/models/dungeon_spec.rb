d253a15559b929e05f21a3943c5beb4213d3472e
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-23 19:50:53 -0600

    Add tests for model, whoops

--- spec/models/dungeon_spec.rb
+++ spec/models/dungeon_spec.rb
@@ -9,16 +9,20 @@ RSpec.describe Dungeon, type: :model do
       @dungeon_1 = Dungeon.create!(name: "Big Bad Things", final_level: true,  difficulty: 5, created_at: 2.days.ago)
       @dungeon_2 = Dungeon.create!(name: "Gross Sewers?", final_level: false,  difficulty: 2, created_at: 3.days.ago)
       @dungeon_3 = Dungeon.create!(name: "Lake of Rot", final_level: false,  difficulty: 6, created_at: 1.day.ago)
-      # @monster_1 = Monster.create!(name: "Goblin", boss: false, health: 15, dungeon_id: dungeon_1.id)
-      # @monster_2 = Monster.create!(name: "Medusa", boss: true, health: 75, dungeon_id: dungeon_2.id)
-      # @monster_3 = Monster.create!(name: "Skeleton", boss: false, health: 25, dungeon_id: dungeon_2.id)
+      @skeleton = @dungeon_1.monsters.create!(name: "Skeleton", boss: false, health: 10)
+      @demi_human = @dungeon_2.monsters.create!(name: "Demi Human", boss: false, health: 25)
+      @malenia = @dungeon_2.monsters.create!(name: "Malenia", boss: true, health: 200)
     end
       
 
     describe "instance methods" do
-      it "#sorted_by_most_recently_created" do
-        require 'pry'; binding.pry
+      it "#sorted_by_most_recent" do
         expect(Dungeon.sort_by_most_recent).to eq([@dungeon_3, @dungeon_1, @dungeon_2])
+      end
+
+      it "monster_counts" do
+        expect(@dungeon_1.monster_count).to eq(1)
+        expect(@dungeon_2.monster_count).to eq(2)
       end
     end
   end

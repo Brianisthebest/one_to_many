b95450949e0700e7f9903b536c62dddb86429ca8
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-03-25 14:22:10 -0600

    Refactor: minor adjustments for readability

--- app/controllers/dungeons_controller.rb
+++ app/controllers/dungeons_controller.rb
@@ -11,7 +11,12 @@ class DungeonsController < ApplicationController
   end
 
   def create
-    new_dungeon = Dungeon.create(name: params[:name], final_level: params[:final_level], difficulty: params[:difficulty])
+    Dungeon.create(name: params[:name], final_level: params[:final_level], difficulty: params[:difficulty])
     redirect_to "/dungeons"
+  end
+
+  def edit
+    edit_dungeon =  @dungeon = Dungeon.find(params[:id])
+    require 'pry'; binding.pry
   end
 end
